52dcd2b625c85a097ec487eab64c8cbba0131476
Albin <aljo940616@gmail.com>
2025-02-09 13:24:03 +0100

    Create models describing the operators data

---
+++ FluxClient/Models/OperatorModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MVVMClient.Models
+{
+    public record OperatorModel
+    {
+        public Guid Id { get; set; }
+        public string? OpId { get; set; }
+        public string? OpName { get; set; }
+        public bool Access1 { get; set; }
+        public bool Access2 { get; set; }
+        public bool Access3 { get; set; }
+        public string? ChangedBy { get; set; }
+        public DateTime ChangedTime { get; set; }
+        public string? Description { get; set; }
+        public bool SelectedToBeDeleted { get; set; } = false;
+    }
+}
+
